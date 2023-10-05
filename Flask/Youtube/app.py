0f9167cbc4c62aace84a772c86c1ab31353f6822
Anuvrat Nehra <84926534+anuvratnehra@users.noreply.github.com>
2023-10-06 00:31:36 +0530

    Updated Flask files

--- Flask/Youtube/app.py
+++ Flask/Youtube/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, flash
 import random
 import re
+import kmeans
 
 app = Flask(__name__)
 app.secret_key = 'some_secret_key'  # Required for flashing messages
@@ -9,6 +10,43 @@ YOUTUBE_LINK_PATTERN = r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    wordsList=None
+    quesList=None
+    if request.method=='POST':
+        wordsOutput=open("kmeansOutput.txt")
+        questionsOut= open('questionsOutput.txt')
+
+        tempList=[]
+        wordsList=[]
+        for line in wordsOutput:
+            if(line=="][\n"):
+                wordsList.append(tempList)
+                tempList=[]
+                continue
+            if(line=="[\n"):
+                continue
+            else:
+                tempList.append(line)
+        wordsList.append(tempList)
+        
+        tempList=[]
+        quesList=[]
+
+        for line in questionsOut:
+            if(line=="][\n"):
+                quesList.append(tempList)
+                tempList=[]
+                continue
+            if(line=="[\n"):
+                continue
+            else:
+                tempList.append(line)
+        quesList.append(tempList)
+        wordsOutput.close()
+        questionsOut.close()
+    return render_template('content.html', text=wordsList, ques=quesList)
+    
+    
     number = None
     energy_message = None
     
@@ -31,3 +69,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
